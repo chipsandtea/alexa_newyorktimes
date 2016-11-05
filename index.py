c588c9a93213d1e919fd121585c68b3ab6ca6217
Christopher Hsiao <hsiao.christopher@gmail.com>
2016-11-05 16:09:33 -0700

    updated index.py

--- index.py
+++ index.py
@@ -1,130 +1,38 @@
-
 """
-This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
-
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
+This is a simple Alexa Skill that reads the top headlines from BBC News.
 """
-
 from __future__ import print_function
 import requests
-
-# --------------- Helpers that build all of the responses ----------------------
-
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+import json
 
 
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
-
-
-# --------------- Functions that control the skill's behavior ------------------
-
-def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
     """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
 
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Good morning! Allow me to read a quick article summary to you!"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
     """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
 
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
 
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
 
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
-
-# --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
@@ -154,14 +62,12 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+    if intent_name == "GetNewsIntent":
+        return get_news(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
+        return get_help_response(intent, session)
+    elif intent_name == "AMAZON.CancelIntent":
+        return get_cancel_response(intent, session)
     else:
         raise ValueError("Invalid intent")
 
@@ -176,31 +82,106 @@ def on_session_ended(session_ended_request, session):
     # add cleanup logic here
 
 
-# --------------- Main handler ------------------
+# --------------- Functions that control the skill's behavior ------------------
 
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
+
+def get_welcome_response():
+    session_attributes = {}
+    card_title = "News Headlines"
+    speech_output = "Hello, I can read you the top news headlines. " \
+                    "For example, you can say, " \
+                    "what's in the news.  " \
+
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Please tell me what news you want. " \
+                    "For example, you can say, " \
+                    "what's in the news.  " \
+
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_help_response(intent, session):
+    session_attributes = session.get('attributes', {})
+    card_title = "Help"
+    speech_output = "I can read you the top news headlines. Just say, " \
+                    "what's in the news." 
+    reprompt_text = "I'm sorry, I didn't hear what you said. " \
+                    "Please tell me if you want news."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_cancel_response(intent, session):
+    session_attributes = {}
+    card_title = "Goodbye"
+    speech_output = "Have a good day, Chris!"
+    reprompt_text = None
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_url(section):
     """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
-
+    Returns the URL for the RSS feed based on the news section
     """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    return 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=4f085be2b93a4c4b8ca57c3d59aa1942'
 
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
 
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+def get_news(intent, session, num_headlines=3):
+    """ Gets the news headlines"""
+    card_title = "News Headlines"
+    session_attributes = session.get('attributes', {})
+
+    url = get_url()
+    r = requests.get(url)
+    parsed = json.loads(r.content)
+    headlines = []
+    for i in range(len(parsed['results'])):
+        headlines.append(parsed['results'][i]['title'])
+
+    speech_output = "Here's what's new in {} news.".format(section)
+    for headline in headlines:
+        speech_output += " " + headline
+
+    reprompt_text = None
+    should_end_session = True
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
--- test.py
+++ test.py
@@ -5,6 +5,8 @@ url = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=4f085be2b93a4c
 
 r = requests.get(url)
 parsed = json.loads(r.content)
-print(parsed)
+headlines = []
 for i in range(len(parsed['results'])):
-	print(parsed['results'][i]['title'])
+	headlines.append(parsed['results'][i]['title'])
+#headlines = [for i in parsed['results'][i]['title']]
+print(headlines)
